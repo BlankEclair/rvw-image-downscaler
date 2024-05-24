d694e9b9deb13d8d11609524168ae0e7a8bb2d93
BlankEclair <blankeclair@waifu.club>
2024-05-24 20:20:17 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+"use strict";
+
+// https://en.wikipedia.org/wiki/Wikipedia:Non-free_content#Image_resolution
+const MAX_MEGAPIXELS = 100000;
+
+/*** DOM ELEMENTS ***/
+let firefoxNote = document.querySelector("#firefoxNote");
+let activateCanvasPermissionPopup = document.querySelector("#activateCanvasPermissionPopup");
+
+let errorBox = document.querySelector("#errorBox");
+let errorPre = document.querySelector("#errorPre");
+
+let imageInput = document.querySelector("[name=imageInput]");
+let downloadDestinationImage = document.querySelector("#downloadDestinationImage");
+let sourceImageMetadata = document.querySelector("#sourceImageMetadata");
+let destinationImageMetadata = document.querySelector("#destinationImageMetadata");
+let destinationImage = document.querySelector("#destinationImage");
+
+/*** ERROR HANDLER ***/
+function handleError(error) {
+    let text = `${error.name}: ${error.message}`;
+    if (error.stack) {
+        text += `\n${error.stack}`;
+    }
+
+    if (errorPre.innerText) {
+        errorPre.innerText += "\n---\n";
+    }
+    errorPre.innerText += text;
+
+    errorBox.classList.remove("hide");
+    throw error;
+}
+
+/*** FUNCTIONS ***/
+function initiateDownload() {
+    let a = document.createElement("a");
+    a.download = "Downscaled - " + imageInput.files[0].name.replace(/\.\w{3,4}$/, "") + ".png";
+    a.href = destinationImage.toDataURL();
+    a.click();
+}
+
+function handleImageInImageInput(autoDownload) {
+    createImageBitmap(imageInput.files[0]).then((image) => {
+        sourceImageMetadata.innerText = `Source image resolution: ${image.width}x${image.height}`;
+        if (image.width * image.height <= MAX_MEGAPIXELS) {
+            destinationImageMetadata.innerText = "No downscale necessary, source image meets resolution requirements";
+            destinationImage.getContext("2d").reset();
+            downloadDestinationImage.disabled = true;
+            return;
+        }
+
+        // https://en.wikipedia.org/wiki/Wikipedia:Non-free_content#Image_resolution
+        let newWidth = Math.floor(Math.sqrt(MAX_MEGAPIXELS * image.width / image.height));
+        let newHeight = Math.floor(image.height * newWidth / image.width);
+        destinationImageMetadata.innerText = `Downscaled image resolution: ${newWidth}x${newHeight}`;
+
+        destinationImage.width = newWidth;
+        destinationImage.height = newHeight;
+        destinationImage.getContext("2d").drawImage(image, 0, 0, newWidth, newHeight);
+
+        downloadDestinationImage.disabled = false;
+        if (autoDownload) {
+            initiateDownload();
+        }
+    }, (error) => {
+        handleError(error);
+    });
+}
+
+/*** DOM EVENT LISTENERS ***/
+if (navigator.userAgent.includes("Firefox/")) {
+    firefoxNote.classList.remove("hide");
+}
+activateCanvasPermissionPopup.addEventListener("click", () => {
+    try {
+        document.createElement("canvas").toDataURL();
+    } catch (error) {
+        handleError(error);
+    }
+});
+
+imageInput.disabled = false;
+imageInput.addEventListener("change", () => {
+    try {
+        handleImageInImageInput(true);
+    } catch (error) {
+        handleError(error);
+    }
+});
+// When the page is refreshed with an image loaded, that image will be prefilled in the input element,
+// but the image isn't downscaled
+if (imageInput.value) {
+    try {
+        handleImageInImageInput(false);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+downloadDestinationImage.addEventListener("click", () => {
+    try {
+        initiateDownload();
+    } catch (error) {
+        handleError(error);
+    }
+});
